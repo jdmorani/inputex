b5e54b9b3769cbbe50ca7c97a65aa7240b8f06b6
Jean-Dominique Morani <jdmorani@gmail.com>
2012-08-06 15:32:20 -0500

    -optimizations to large tables -add support for removing or adding all fields to a table

--- js/Table.js
+++ js/Table.js
@@ -1,179 +1,224 @@
-(function() {
+(function () {
 
-  var lang = YAHOO.lang,
-      Dom = YAHOO.util.Dom,
-      Event = YAHOO.util.Event;
-
-  /**
-   * Handle a table of fields
-   * @class inputEx.Table
-   * @extends inputEx.Group
-   * @constructor
-   * @param {Object} options The following options are added for Tables and subclasses:
-   * <ul>
-   *   <li>fields: Array of input fields declared like { label: 'Enter the value:' , type: 'text' or fieldClass: inputEx.Field, optional: true/false, ... }</li>
-   *   <li>legend: The legend for the fieldset (default is an empty string)</li>
-   *   <li>collapsible: Boolean to make the table collapsible (default is false)</li>
-   *   <li>collapsed: If collapsible only, will be collapsed at creation (default is false)</li>
-   *   <li>flatten:</li>
-   * </ul>
-   */
-  inputEx.Table = function(options) {
-    inputEx.Table.superclass.constructor.call(this, options);
-
-    // Run default field interactions (if setValue has not been called before)
-    if (!this.options.value) {
-      this.runFieldsInteractions();
-    }
-  };
-
-  lang.extend(inputEx.Table, inputEx.Group, {
+    var lang = YAHOO.lang,
+        Dom = YAHOO.util.Dom,
+        Event = YAHOO.util.Event;
 
     /**
-     * Adds some options: legend, collapsible, fields...
-     * @param {Object} options Options object as passed to the constructor
+     * Handle a table of fields
+     * @class inputEx.Table
+     * @extends inputEx.Group
+     * @constructor
+     * @param {Object} options The following options are added for Tables and subclasses:
+     * <ul>
+     *   <li>fields: Array of input fields declared like { label: 'Enter the value:' , type: 'text' or fieldClass: inputEx.Field, optional: true/false, ... }</li>
+     *   <li>legend: The legend for the fieldset (default is an empty string)</li>
+     *   <li>collapsible: Boolean to make the table collapsible (default is false)</li>
+     *   <li>collapsed: If collapsible only, will be collapsed at creation (default is false)</li>
+     *   <li>flatten:</li>
+     * </ul>
      */
-    setOptions: function(options) {
-      inputEx.Table.superclass.setOptions.call(this, options);
+    inputEx.Table = function (options) {
+        inputEx.Table.superclass.constructor.call(this, options);
 
-      this.options.className = options.className || 'inputEx-Group';
-
-      this.options.fields = options.fields;
-
-      this.options.flatten = options.flatten;
-
-      this.options.legend = options.legend || '';
-
-      this.options.collapsible = lang.isUndefined(options.collapsible) ? false : options.collapsible;
-      this.options.collapsed = lang.isUndefined(options.collapsed) ? false : options.collapsed;
-
-      this.options.disabled = lang.isUndefined(options.disabled) ? false : options.disabled;
-
-      // Array containing the list of the field instances
-      this.inputs = [];
-
-      // Associative array containing the field instances by names
-      this.inputsNames = {};
-
-      // associate the table with the initial fields list
-      if (typeof this.options.fields !== 'undefined' && this.options.fields.length == 0 && this.numberOfFieldsInTable() > 0) {
-        this.updateFieldList();
-      }
-      
-      this.subscribeToTableDidChangeEvent();
-    },
-
-    subscribeToTableDidChangeEvent: function() {
-      if (this.parentField && this.parentField.group && this.parentField.group.type == 'table') {
-        for (var i = 0; i < this.parentField.group.inputs.length; i++) {
-          if (this.parentField.group.inputs[i].type == 'dynamictable') {
-            // subscribe to the parent field tableDidChangeEvent
-            this.options.parentTableDidChange = this.parentField.group.inputs[i].options.tableDidChangeEvt;
-            this.options.parentTableDidChange.subscribe(this.onTableDidChange, this, true);
-          }
+        // Run default field interactions (if setValue has not been called before)
+        if (!this.options.value) {
+            this.runFieldsInteractions();
         }
-      }
-    },
+    };
 
-    onTableDidChange: function(e, args) {
-      this.options.name = args[0];
-      this.updateFieldList();
-    },
+    lang.extend(inputEx.Table, inputEx.Group, {
 
-    numberOfFieldsInTable: function(){
-      if(typeof inputEx.TablesFields === 'undefined'){
-        return 0;
-      }
-      for (var i = 0; i < inputEx.TablesFields.length; i++) {
-        if (inputEx.TablesFields[i].table.key == this.options.name){
-          return(inputEx.TablesFields[i].table.fields.length);
-        }
-      }
-    },
+        /**
+         * Adds some options: legend, collapsible, fields...
+         * @param {Object} options Options object as passed to the constructor
+         */
+        setOptions:function (options) {
+            inputEx.Table.superclass.setOptions.call(this, options);
 
-    /**
-     * Retrieve the list of tables to be used to populate
-     * the select field
-     */
-    updateFieldList: function() {
-      try {
-        var fields = [];
-        this.setFieldsList(this.parentField.group, []);
-        for (var i = 0; i < inputEx.TablesFields.length; i++) {
-          if (inputEx.TablesFields[i].table.key == this.options.name) {
-            for (var j = 0; j < inputEx.TablesFields[i].table.fields.length; j++) {
+            this.options.className = options.className || 'inputEx-Group';
 
-              fields.push({
-                label: inputEx.TablesFields[i].table.fields[j].name,
-                name: inputEx.TablesFields[i].table.fields[j].name + '@_@@_@' + inputEx.TablesFields[i].table.fields[j].key,
-                value: inputEx.TablesFields[i].table.fields[j].default_value,
-                type: this.getFieldType(inputEx.TablesFields[i].table.fields[j].field_type)
-              });
+            this.options.fields = options.fields;
+
+            this.options.flatten = options.flatten;
+
+            this.options.legend = options.legend || '';
+
+            this.options.collapsible = lang.isUndefined(options.collapsible) ? false : options.collapsible;
+            this.options.collapsed = lang.isUndefined(options.collapsed) ? false : options.collapsed;
+
+            this.options.disabled = lang.isUndefined(options.disabled) ? false : options.disabled;
+
+            // Array containing the list of the field instances
+            this.inputs = [];
+
+            // Associative array containing the field instances by names
+            this.inputsNames = {};
+
+            // associate the table with the initial fields list
+            if (typeof this.options.fields !== 'undefined' && this.options.fields.length == 0 && this.numberOfFieldsInTable() > 0) {
+                this.updateFieldList();
             }
-            break;
-          }
+
+            this.subscribeToTableDidChangeEvent();
+        },
+
+        initEvents:function(){
+            this.addAllFieldsElement = document.getElementsByName("addallfields");
+            this.removeAllFieldsElement = document.getElementsByName("removeallfields");
+            Event.addListener(this.addAllFieldsElement,"click",function(e, obj){
+                obj.updateFieldList(true);
+            }, this)
+
+            Event.addListener(this.removeAllFieldsElement,"click",function(e, obj){
+                obj.setFieldsList(obj.parentField.group, []);
+            }, this)
+        },
+
+        subscribeToTableDidChangeEvent:function () {
+            if (this.parentField && this.parentField.group && this.parentField.group.type == 'table') {
+                for (var i = 0; i < this.parentField.group.inputs.length; i++) {
+                    if (this.parentField.group.inputs[i].type == 'dynamictable') {
+                        // subscribe to the parent field tableDidChangeEvent
+                        this.options.parentTableDidChange = this.parentField.group.inputs[i].options.tableDidChangeEvt;
+                        this.options.parentTableDidChange.subscribe(this.onTableDidChange, this, true);
+                    }
+                }
+            }
+        },
+
+        onTableDidChange:function (e, args) {
+            this.options.name = args[0];
+            this.updateFieldList();
+        },
+
+        numberOfFieldsInTable:function () {
+            if (typeof inputEx.TablesFields === 'undefined') {
+                return 0;
+            }
+            for (var i = 0; i < inputEx.TablesFields.length; i++) {
+                if (inputEx.TablesFields[i].table.key == this.options.name) {
+                    return(inputEx.TablesFields[i].table.fields.length);
+                }
+            }
+        },
+
+        /**
+         * Retrieve the list of tables to be used to populate
+         * the select field
+         */
+        updateFieldList:function (addAllFields) {
+            try {
+                var fields = [];
+                this.setFieldsList(this.parentField.group, []);
+                for (var i = 0; i < inputEx.TablesFields.length; i++) {
+                    if (inputEx.TablesFields[i].table.key == this.options.name) {
+                        for (var j = 0; j < inputEx.TablesFields[i].table.fields.length; j++) {
+
+                            fields.push({
+                                label:inputEx.TablesFields[i].table.fields[j].name,
+                                name:inputEx.TablesFields[i].table.fields[j].name + '@_@@_@' + inputEx.TablesFields[i].table.fields[j].key,
+                                value:inputEx.TablesFields[i].table.fields[j].default_value,
+                                type:this.getFieldType(inputEx.TablesFields[i].table.fields[j].field_type)
+                            });
+                        }
+                        break;
+                    }
+                }
+                if (addAllFields == true) this.setFieldsList(this.parentField.group, fields);
+            } catch (err) {
+                console.log("inputEx.TablesFields is undefined. - " + err)
+            }
+        },
+
+        addField:function (fieldOptions) {
+            var field = this.renderField(fieldOptions);
+            this.fieldset.appendChild(field.getEl());
+        },
+
+        destroy:function () {
+            if (this.options.parentTableDidChange) this.options.parentTableDidChange.unsubscribe(this.onTableDidChange, this);
+
+            // Destroy group itself
+            inputEx.Table.superclass.destroy.call(this);
+
+        },
+
+        setFieldsList:function (group, fields) {
+            for (var i = 0; i < group.inputs.length; i++) {
+                if (group.inputs[i].type == 'list') {
+                    group.inputs[i].setValue(fields);
+                }
+            }
+        },
+
+        destroy: function(){
+            if(typeof this.addAllFieldsElement !== 'undefined'){
+                Event.removeListener(this.addAllFieldsElement, "click");
+            }
+
+            if(typeof this.removeAllFieldsElement !== 'undefined'){
+                Event.removeListener(this.removeAllFieldsElement, "click");
+            }
         }
-        if (fields.length > 0) this.setFieldsList(this.parentField.group, fields);
-      } catch (err) {
-        console.log("inputEx.TablesFields is undefined. - " + err)
-      }
-    },
+    });
 
-    addField: function(fieldOptions) {      
-      var field = this.renderField(fieldOptions);
-      this.fieldset.appendChild(field.getEl());
-    },
-
-    destroy: function() {
-      if (this.options.parentTableDidChange) this.options.parentTableDidChange.unsubscribe(this.onTableDidChange, this);
-
-      // Destroy group itself      
-      inputEx.Table.superclass.destroy.call(this);
-
-    },
-
-    setFieldsList: function(group, fields) {
-      for (var i = 0; i < group.inputs.length; i++) {
-        if (group.inputs[i].type == 'list') {
-          group.inputs[i].setValue(fields);
+    // Register this class as "table" type
+    inputEx.registerType("table", inputEx.Table, [
+        {
+            type:"dynamictable",
+            label:"Name",
+            name:"name",
+            choices:[],
+            required:true
+        },
+        {
+            type:'string',
+            label:'Legend',
+            name:'legend'
+        },
+        {
+            type:'boolean',
+            label:'Collapsible',
+            name:'collapsible',
+            value:false
+        },
+        {
+            type:'boolean',
+            label:'Collapsed',
+            name:'collapsed',
+            value:false
+        },
+        {
+            type:'list',
+            label:'Fields',
+            name:'fields',
+            sortable:'true',
+            align: 'true',
+            elementType:{
+                type:'type'
+            }
+        },
+        {
+            type:'button',
+            label:'Add all fields',
+            name:'addallfields',
+            align: 'true'
+        },
+        {
+            type:'button',
+            label:'Remove all fields',
+            name:'removeallfields',
+            align: 'true'
+        },
+        {
+            type:'string',
+            name: 'spacer',
+            hide: 'true',
+            newline: 'true'
         }
-      }
-    }
 
-
-  });
-
-
-  // Register this class as "table" type
-  inputEx.registerType("table", inputEx.Table, [{
-    type: "dynamictable",
-    label: "Name",
-    name: "name",
-    choices: [],
-    required: true
-  }, {
-    type: 'string',
-    label: 'Legend',
-    name: 'legend'
-  }, {
-    type: 'boolean',
-    label: 'Collapsible',
-    name: 'collapsible',
-    value: false
-  }, {
-    type: 'boolean',
-    label: 'Collapsed',
-    name: 'collapsed',
-    value: false
-  }, {
-    type: 'list',
-    label: 'Fields',
-    name: 'fields',
-    sortable: 'true',
-    elementType: {
-      type: 'type'
-    }
-  }], true);
+    ], true);
 
 
 })();
