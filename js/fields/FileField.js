43041b4ae1c96a117d6efe6168278a497475d26a
Jean-Dominique Morani <jdmorani@gmail.com>
2012-09-11 12:25:05 -0500

    add support for attachments

--- js/fields/FileField.js
+++ js/fields/FileField.js
@@ -1,54 +1,69 @@
-(function() {
+(function () {
 
-   var Event = YAHOO.util.Event;
+    var Event = YAHOO.util.Event;
 
-/**
- * Create a file input
- * @class inputEx.FileField
- * @extends inputEx.Field
- * @constructor
- * @param {Object} options Added options:
- * <ul>
- * </ul>
- */
-inputEx.FileField = function(options) {
-	inputEx.FileField.superclass.constructor.call(this,options);
-};
-YAHOO.lang.extend(inputEx.FileField, inputEx.Field, {
-	
-   /**
-    * Adds size and accept options
-    * @param {Object} options Options object as passed to the constructor
-    */
-   setOptions: function(options) {
-		inputEx.FileField.superclass.setOptions.call(this, options);
-		this.options.size = options.size;
-		this.options.accept = options.accept;
-	},
-	
-   /**
-    * Render an 'INPUT' DOM node
-    */
-   renderComponent: function() {
-      
-      // Attributes of the input field
-      var attributes = {};
-      attributes.id = this.divEl.id?this.divEl.id+'-field':YAHOO.util.Dom.generateId();
-      attributes.type = "file";
-      if(this.options.name) attributes.name = this.options.name;
-   	if(this.options.size) attributes.size = this.options.size;
-   	if(this.options.accept) attributes.accept = this.options.accept;
+    /**
+     * Create a file input
+     * @class inputEx.FileField
+     * @extends inputEx.Field
+     * @constructor
+     * @param {Object} options Added options:
+     * <ul>
+     * </ul>
+     */
+    inputEx.FileField = function (options) {
+        inputEx.FileField.superclass.constructor.call(this, options);
+    };
+    YAHOO.lang.extend(inputEx.FileField, inputEx.Field, {
 
-      // Create the node
-      this.el = inputEx.cn('input', attributes);
-      
-      // Append it to the main element
-      this.fieldContainer.appendChild(this.el);
-   }
+        /**
+         * Adds size and accept options
+         * @param {Object} options Options object as passed to the constructor
+         */
+        setOptions:function (options) {
+            inputEx.FileField.superclass.setOptions.call(this, options);
+            this.options.size = options.size;
+            this.options.accept = options.accept;
+            this.options.multiple = options.multiple;
+        },
 
-});
+        /**
+         * Render an 'INPUT' DOM node
+         */
+        renderComponent:function () {
 
-// Register this class as "file" type
-inputEx.registerType("file", inputEx.FileField);
+            // Attributes of the input field
+            var attributes = {};
+            attributes.id = this.divEl.id ? this.divEl.id + '-field' : YAHOO.util.Dom.generateId();
+            attributes.type = "file";
+            if (this.options.name) attributes.name = this.options.name;
+            if (this.options.size) attributes.size = this.options.size;
+            if (this.options.accept) attributes.accept = this.options.accept;
+            if (this.options.multiple) attributes.multiple = this.options.multiple;
+
+            // Create the node
+            this.el = inputEx.cn('input', attributes);
+
+            // Append it to the main element
+            this.fieldContainer.appendChild(this.el);
+        }
+
+    });
+
+// Register this class as "url" type
+    inputEx.registerType("file", inputEx.FileField, [
+        {
+            type:"accept",
+            label:"File Format",
+            name:"accept",
+            required:false
+        },
+        {
+            type:"boolean",
+            label:"Multiple",
+            name:"multiple",
+            required:false
+        }
+    ], false);
 
 })();
